9c47090cb71930b828ad139df1e8344c20c8f4c4
red3aaa <m15013049151@gmail.com>
2025-05-22 17:05:08 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cloudflared service install ${cft}
+sudo cloudflared service install ${cft}
 
 
 dir_shell=/ql/shell
@@ -22,7 +22,7 @@ echo -e "======================2. 安装依赖========================\n"
 patch_version
 
 echo -e "======================3. 启动nginx========================\n"
-nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx2.conf
+nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
 echo -e "nginx启动成功...\n"
 
 echo -e "======================4. 启动pm2服务========================\n"
